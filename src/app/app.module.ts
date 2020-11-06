3fac205a70fb12d2dccc439dc774d29b46f6b383
TylerBoyd <tboyd1898@gmail.com>
2020-11-06 17:00:58 -0600

    added mongoose and added database connection string

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from '../products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://tboyd:omegarubygroudon@cluster0.08gj6.mongodb.net/nestjs-demo?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
